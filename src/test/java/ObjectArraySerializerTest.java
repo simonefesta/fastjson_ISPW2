854f8ec8f842132d97eb4ac498cd0dbb1e1a0995
Simone Festa <55951548+simonefesta@users.noreply.github.com>
2022-06-01 18:25:09 +0200

    added test

--- src/test/java/ObjectArraySerializerTest.java
+++ src/test/java/ObjectArraySerializerTest.java
@@ -11,48 +11,56 @@ import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Objects;
 
 @RunWith(Parameterized.class)
 public class ObjectArraySerializerTest extends TestCase {
 
- private SerializeWriter out;
- private Object[] obj;
- private String expected;
+    private SerializeWriter out;
+    private Object[] obj;
+    private String expected;
 
-//costruttore
-public ObjectArraySerializerTest(SerializeWriter out, Object[] obj, String expected) {
-    configure(out, obj,expected); //nel costruttore uso configure.
-}
 
-//configure
-    private void configure(SerializeWriter out, Object[] obj,String expected)
-    {
+    //costruttore
+    public ObjectArraySerializerTest(SerializeWriter out, Object[] obj, String expected) {
+        configure(out, obj, expected); //nel costruttore uso configure.
+    }
+
+    //configure
+    private void configure(SerializeWriter out, Object[] obj, String expected) {
         this.out = out;
         this.obj = obj;
         this.expected = expected;
     }
 
-
     @Parameterized.Parameters
     public static Collection<Object[]> getTestParameters() {
         return Arrays.asList(new Object[][]{
-                {new SerializeWriter(1), new Object[] { "a12", "b34" },"[\"a12\",\"b34\"]"},
-                {new SerializeWriter(1), new Object[] {},"[]"},
-                {new SerializeWriter(1), new Object[] {null,null},"[null,null]"}
-
+                // serializeWriter          Object[]                     Expected
+                {new SerializeWriter(1), new Object[]{"a12", "b34"}, "[\"a12\",\"b34\"]"},
+                {new SerializeWriter(1), new Object[]{}, "[]"},
+                {new SerializeWriter(1), new Object[]{null, null}, "[null,null]"},
         });
     }
-    //{JSON.toJSONString(new Object[] { null, null }, false),"[null,null]"} andrebbe tra i test, ma ha una config diversa.
 
-        @Test
-        public void test() throws Exception {
+    @Test
+    public void test() {
 
-            JSONSerializer.write(out, obj);
+        JSONSerializer.write(out, obj);
 
-            Assert.assertEquals(this.expected, out.toString());
+        Assert.assertEquals(this.expected, out.toString());
 
+    }
+
+
+    @Test
+    public void test1() {
+        if (Objects.equals(obj, new Object[]{null, null})) {
+            Assert.assertEquals(expected, JSON.toJSONString(obj, false));
         }
+    }
 
+}
 /*
     public void test_0() throws Exception {
         SerializeWriter out = new SerializeWriter(1);
@@ -84,4 +92,3 @@ public ObjectArraySerializerTest(SerializeWriter out, Object[] obj, String expec
 
 
  */
-}
